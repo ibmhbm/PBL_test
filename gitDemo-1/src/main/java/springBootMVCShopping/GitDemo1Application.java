8eec72580ff449d8143e7dabaa67d40326d60cf2
HKEDU <HKEDU@DESKTOP-U8HF6I9>
2023-11-13 15:15:33 +0900

    test

---
+++ gitDemo-1/src/main/java/springBootMVCShopping/GitDemo1Application.java
@@ -0,0 +1,13 @@
+package springBootMVCShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemo1Application.class, args);
+	}
+
+}
